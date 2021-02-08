b1ba0ffb4fb6e4070d40d179cdfb4de4c91f0a52
Carmenprz <carmenp.gomez99@gmail.com>
2021-02-08 11:32:48 +0100

    React proyect twit started

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+const Header = () => {
+    return (
+        <div>
+            <h2>
+                Soy el header
+            </h2>
+        </div>
+    )
+};
+
+export default Header; 
