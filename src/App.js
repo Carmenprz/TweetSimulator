b7ae502f49db482fca3edb72e75f8cbd0efe8acd
Carmenprz <carmenp.gomez99@gmail.com>
2021-03-02 11:44:08 +0100

    tweets array saved in local storage and modal snackbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import './App.scss';
-import { Container } from '@material-ui/core/'
+import { Container, Snackbar } from '@material-ui/core/'
 import { Header } from './components/Header/Header'
 import { SendTweet } from './components/SendTweet/SendTweet'
+import { TWEETS_STORAGE } from './utils/constants';
 
 function App() {
+
+  const [ toastProps, setToastProps ] = useState({
+    open: false,
+    text: null
+  });
+
+  const [ allTweets, setAllTweets ] = useState([]); 
+
+  useEffect(() => {
+    const allTweetsStorage = localStorage.getItem(TWEETS_STORAGE);
+    const allTweetsArray = JSON.parse(allTweetsStorage);
+    setAllTweets(allTweetsArray);
+  }, []);
+
+  console.log(allTweets);
+
   return (
     <Container className="tweets-simulator" maxWidth={false}>
       <Header />
-      <SendTweet />
+      <SendTweet setToastProps={setToastProps} allTweets={allTweets}/>
+      
+      <Snackbar 
+        anchorOrigin={{
+          vertical:"top",
+          horizontal:"right"
+        }}
+        open={toastProps.open}
+        //hacer el on close
+        autoHideDuration={1000}
+        message={<span id="message-id">{toastProps.text}</span>}
+      />
     </Container>
   );
 }
--- src/components/SendTweet/SendTweet.js
+++ src/components/SendTweet/SendTweet.js
@@ -8,7 +8,8 @@ import moment from 'moment';
 
 import './SendTweet.scss' 
 
-export const SendTweet = () => {
+export const SendTweet = (props) => {
+    const {setToastProps, allTweets} = props;
     const [ isOpenModal, setIsOpenModal ] = useState(false);
 
     const openModal = () => {
@@ -26,13 +27,25 @@ export const SendTweet = () => {
         const { name, tweet } = formValaue;
         let allTweetsArray = []; 
 
+        if (allTweets) {
+            allTweetsArray = allTweets;
+        } 
+
         if(!name || !tweet) {
-            console.log('WARNING: Todos los campos son obligatorios');
+            // console.log('WARNING: Todos los campos son obligatorios');
+            setToastProps({
+                open: true, 
+                text: 'WARNING: Todos los campos son obligatorios'
+            });
         } else {
             formValaue.time = moment(); 
             allTweetsArray.push(formValaue);
             localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweetsArray));
             console.log ('tweet enviado correctamente'); 
+            setToastProps({
+                open: true, 
+                text: 'Tweet enviado correctamente'
+            });
             closeModal();
         }
 
